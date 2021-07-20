dab7dc223fbca2400da1ab6cade7dc1e6790d8a0
imadcollin <imad.collin@gmail.com>
2021-07-20 18:14:17 +0200

    Add some styling for header

--- compnents/Header.js
+++ compnents/Header.js
@@ -1,8 +1,25 @@
+/* eslint-disable */
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 const Header = () => {
-  return <Text>Header Compnent</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Header Compnent</Text>
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  text: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+});
 export default Header;
