74b40162b80e7bd91197ba7713c56d72040d3aec
imadcollin <imad.collin@gmail.com>
2021-07-21 17:34:08 +0200

    Add item compnent

---
+++ compnents/Item.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react'; 
+import {View, Text} from 'react-native';
+
+const Item = () => {
+  return (
+    <View>
+      <Text> Hello from Item</Text>
+    </View>
+  );
+};
+
+export default Item;
