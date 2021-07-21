4880df00c92e276a7d3fcf6edee9d809bd515e8a
imadcollin <imad.collin@gmail.com>
2021-07-21 17:33:23 +0200

    Add image to header

--- compnents/Header.js
+++ compnents/Header.js
@@ -1,10 +1,13 @@
 /* eslint-disable */
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, ImageBackground} from 'react-native';
 
 const Header = () => {
   return (
     <View style={styles.container}>
+      <ImageBackground
+        source={require('./../assets/images/1.jpg')}
+        style={styles.img}></ImageBackground>
       <Text style={styles.text}>Header Compnent</Text>
     </View>
   );
@@ -12,14 +15,22 @@ const Header = () => {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 10,
+    width: '100%',
+    height: '50%',
   },
   text: {
     color: '#000',
     fontWeight: 'bold',
     fontSize: 24,
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
   },
 });
 export default Header;
