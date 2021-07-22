d351a82a36d81ad3041a4b743e9e110159e6d7aa
imadcollin <imad.collin@gmail.com>
2021-07-22 15:47:28 +0200

    Break down Item from style

---
+++ compnents/Items/Item.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import ItemStyles from './ItemStyle';
+const Item = () => {
+  return (
+    <View style={ItemStyles.container}>
+      <Text style={ItemStyles.title}> Hello from Item</Text>
+    </View>
+  );
+};
+
+export default Item;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ compnents/Items/ItemStyle.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const ItemStyles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: 'black',
+  },
+  title: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'normal',
+    textAlign: 'center',
+  },
+});
+
+export default ItemStyles;
