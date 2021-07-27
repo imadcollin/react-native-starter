8bfbbfa9f5d5c8e82708f7a2f4d748fcc019e33e
imadcollin <imad.collin@gmail.com>
2021-07-27 11:56:52 +0200

    Move constants to own folder

---
+++ src/constants/color.js
@@ -0,0 +1,4 @@
+export const Colors = {
+    primary: '#fff',
+    secondary: '#000',
+  }
---
+++ src/constants/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    id: 1,
+    name: 'item1',
+    text: 'this is the item1'
+  },
+  {
+    id: 2,
+    name: 'item2',
+    text: 'this is item 2'
+  },
+  {
+    id: 3,
+    name: 'item3',
+    text: 'this it item 3'
+  },
+];
+export default data;
